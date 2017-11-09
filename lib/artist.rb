ec3f1ed31fa9443811c0133de99b59034f6ae0ab
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 19:00:00 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+  @@song_count
+  attr_accessor :name
+  
+  def initialize(name)
+      @name = name
+      @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+  
+  def add_song_by_name(song, name)
+    song = Song.new(name)
+    self.add_song(song)
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
